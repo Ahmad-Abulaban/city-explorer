87c1ac766420983926e79cbef392a13dd75d448f
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-09-21 17:30:29 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -10,19 +10,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       locationResult: {},
-      searchQuery: '',
+      WeatherResult: [],
+      searchQuery: "",
       showLocInfo: false,
-      cityInfo: {}
     };
-  }
-
-
-  getInfo = async ()=> {
-    let location = `${process.env.REACT_APP_SERVER_LINK}/data/weather.json/timezone`
-    let AllData = await axios.get(location)
-    this.setState({
-      cityInfo: AllData.data
-    })
   }
 
   getLocFun = async (e) => {
@@ -36,50 +27,93 @@ class App extends React.Component {
 
     let locResult = await axios.get(reqUrl);
 
+    let weatherUrl = `http://localhost:3005/weather?SearchQuery=${this.state.searchQuery}`;
+
+    let weatherResult = await axios.get(weatherUrl);
+
     this.setState({
       locationResult: locResult.data[0],
+      WeatherResult: weatherResult.data,
       showLocInfo: true,
     });
+    console.log(this.state.WeatherResult);
   };
 
   render() {
     return (
       <div>
-        <header style={{backgroundColor: "gray", padding:'15px'}}>
-        <h2>City-Explorer</h2>
+        <header style={{ backgroundColor: "gray", padding: "15px" }}>
+          <h2>City-Explorer</h2>
         </header>
-        <main style={{backgroundColor: "lightgray" , marginTop: '-25px', paddingBottom: '25px', width: '100%', minHeight: '832px'}}>
-        <Form onSubmit={this.getLocFun} style={{ width: "25rem" , marginLeft: '25px', marginTop: '25px', paddingBottom: '50px', paddingTop: '50px'}} >
-          <Form.Group className="mb-3" controlId="SearchForLocation">
-            <Form.Label>Location City</Form.Label>
-            <Form.Control type="text" name='city' placeholder="Enter Location City" />
-          </Form.Group>
-          <Button type="submit" variant="primary">
-            Explore!
-          </Button>
-        </Form>
+        <main
+          style={{
+            backgroundColor: "lightgray",
+            marginTop: "-25px",
+            paddingBottom: "25px",
+            width: "100%",
+            minHeight: "832px",
+          }}
+        >
+          <Form
+            onSubmit={this.getLocFun}
+            style={{
+              width: "25rem",
+              marginLeft: "25px",
+              marginTop: "25px",
+              paddingBottom: "50px",
+              paddingTop: "50px",
+            }}
+          >
+            <Form.Group className="mb-3" controlId="SearchForLocation">
+              <Form.Label>Location City</Form.Label>
+              <Form.Control
+                type="text"
+                name="city"
+                placeholder="Enter Location City"
+              />
+            </Form.Group>
+            <Button type="submit" variant="primary">
+              Explore!
+            </Button>
+          </Form>
 
-        {this.state.showLocInfo &&
-          <>
-            <Card style={{ width: "18rem" , border: '2px solid red', marginLeft: '25px', marginTop: '25px', marginBottom: '25px'}}>
-              <Card.Body>
-                <Card.Title>Location City</Card.Title>
-                <Card.Text>
-                  <p>latitude: {this.state.locationResult.lat}</p>
-                  <p>longitude: {this.state.locationResult.lon} </p>
-                </Card.Text>
-                <Card.Img
-                  variant="top"
-                  src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`}
-                />
-              </Card.Body>
-            </Card>
-          </>
-        }
+          {this.state.showLocInfo && (
+            <>
+              <Card
+                style={{
+                  width: "25rem",
+                  border: "2px solid red",
+                  marginLeft: "25px",
+                  marginTop: "25px",
+                  marginBottom: "25px",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>Location City</Card.Title>
+                  <Card.Text>
+                    <p>latitude: {this.state.locationResult.lat}</p>
+                    <p>longitude: {this.state.locationResult.lon} </p>
 
-        <p>{this.state.cityInfo.name}</p>
+                    {this.state.WeatherResult.map((ele) => {
+                      return (
+                        <>
+                          <p>date: {ele.date}</p>
+                          <p>
+                            description: {ele.description}</p>
+                        </>
+                      );
+                    })}
+                  </Card.Text>
+                  <Card.Img
+                    variant="top"
+                    src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_SERVER_LINK}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`}
+                  />
+                </Card.Body>
+              </Card>
+            </>
+          )}
         </main>
-        <footer style={{backgroundColor: "gray" , padding:'15px'}}>
+        <footer style={{ backgroundColor: "gray", padding: "15px" }}>
           <h3>Create by Ahmad Abulaban</h3>
         </footer>
       </div>
