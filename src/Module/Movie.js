0eb0681c458b53f44e0eeab112db5feb0a9b5efe
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-09-25 00:46:08 +0300

    Done

--- src/Module/Movie.js
+++ src/Module/Movie.js
@@ -4,18 +4,30 @@ import { Table } from "react-bootstrap";
 class Movie extends React.Component {
   render() {
     return (
-
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>MovieTitle</th>
+            <th style={{ textAlign: "center" }}>Movies</th>
           </tr>
         </thead>
         <tbody>
           {this.props.MovieResult.map((ele) => {
             return (
-              <tr>
-                <td>{ele.title}</td>
+              <tr style={{ textAlign: "center"}}>
+                <br />
+                <tr>{ele.title}</tr>
+                <br />
+                <tr>{ele.overview}</tr>
+                <br />
+                <tr>{ele.averageVotes}</tr>
+                <br />
+                <tr>{ele.totalVotes}</tr>  
+                <img style={{ width: "15rem" }} src={ele.imageUrl} />
+                <tr>{ele.popularity}</tr>
+                <br />
+                <tr>{ele.releasedOn}</tr>
+                <br />
+                <br />
               </tr>
             );
           })}
